fd19567d6176b8adcf28ec86d65ae340cd81f2b1
Shoham Peller <shohamp@gmail.com>
2016-02-07 02:12:39 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='culour',
+    packages=['culour'],
+    version='0.1',
+    description='Print colored strings in a curses window',
+    author='Shoham Peller',
+    author_email='shohamp@gmail.com',
+    url='https://github.com/shohamp/culour',
+    download_url='https://github.com/shohamp/culour/tarball/0.1',
+    keywords=['curses', 'color', 'colour'],
+    classifiers=[],
+)
